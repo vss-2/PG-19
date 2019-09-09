3486de8bdc1355b4ffcf9703d02a67fe6fbb6b61
Vitor Sousa Silva <vss2@ug3c16.windows.cin.ufpe.br>
2019-09-09 14:04:56 -0300

    Initial commit: fix far, fix MinGW on Linux

---
+++ camera.h
@@ -0,0 +1,90 @@
+#ifndef CAMERAH
+#define CAMERAH
+
+#include "vec3.h"
+#include "vec2.h"
+#include "matrix44.h"
+#include "object.h"
+
+#ifdef _WIN32 || WIN32
+	#include <SDL.h>
+#elif defined(__unix__)
+	#include <SDL2/SDL.h>
+#endif
+
+const int WIDTH = 600;
+const int HEIGHT = 400;
+
+class camera
+{
+public:
+    int imgWidth, imgHeight;
+    float fov, _far, _near;
+    float bottom, left, top, right;
+    matrix44 camToWorld;
+    matrix44 worldToCamera;
+
+	vec3 _from, _at, _up;
+    vec3 axisX, axisY, axisZ;
+
+public:
+    camera();
+    camera(const vec3 &from, const vec3 &at, const vec3 &up,
+           const float &f, const float &n, const float &a,
+           const int &iwidth, const int &iheight): 
+           fov(f), _near(n), _far(a), imgWidth(iwidth), imgHeight(iheight),
+		   _from(from), _at(at), _up(up)
+           {
+                look_at(from, at, up);
+           }
+
+    void look_at(const vec3 &from, const vec3 &at, const vec3 &up)
+    {
+        camToWorld = matrix44(
+            axisX.x(), axisX.y(), axisX.z(), 0.0,
+            axisY.x(), axisY.y(), axisY.z(), 0.0,
+            axisZ.x(), axisZ.y(), axisZ.z(), 0.0,
+               from.x,    from.y,    from.z, 1.0
+        );
+
+        matrix44 matrizWorldToCamera = camToWorld.inverse;
+    }
+
+    bool compute_pixel_coordinates(const vec3 &pWorld, vec2 &pRaster) 
+    { 
+        return false; // Retornar verdadeiro se o ponto pode ser visto
+    }
+
+    void render_scene( std::vector<Obj> objs, SDL_Renderer* renderer) {
+
+        vec3 light(0.0f, 0.0f, -1.0f);
+        light.make_unit_vector();
+
+        for (auto obj : objs){
+            for (int i = 0; i < obj.mesh.tris.size(); i++)
+            {
+                vec2 praster1;
+                vec2 praster2;
+                vec2 praster3;
+
+                vec3 col(255, 255, 255);
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+                bool v1, v2, v3;
+                v1 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[0].pos, praster1);
+                v2 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[1].pos, praster2);
+                v3 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[2].pos, praster3);
+
+                if(v1 && v2)
+                    SDL_RenderDrawLine(renderer, praster1.x(), praster1.y(), praster2.x(), praster2.y());
+                if(v1 && v3)
+                    SDL_RenderDrawLine(renderer, praster1.x(), praster1.y(), praster3.x(), praster3.y());
+                if(v2 && v3)
+                    SDL_RenderDrawLine(renderer, praster2.x(), praster2.y(), praster3.x(), praster3.y());
+            }
+        }
+    }
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,139 @@
+
+#include <string>
+#include <math.h>
+#include "camera.h" 
+
+#include "ImGUI/imgui_sdl.h"
+#include "ImGUI/imgui.h"
+
+
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+
+		SDL_Window* window = SDL_CreateWindow("Projeto PG - Grupo X", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+        if ( window && renderer ) {
+            SDL_bool done = SDL_FALSE;
+			SDL_SetRelativeMouseMode(SDL_FALSE);
+            
+			std::vector<Obj> objects;
+            objects.push_back( Obj("./objects/monkey_smooth.obj") );
+
+			ImGui::CreateContext();
+			ImGuiSDL::Initialize(renderer, WIDTH, HEIGHT);
+
+            camera cam(vec3(0, 0, 5), vec3(0, 0, -1), vec3(0, 1, 0), 90.0f, 1.f, 1000, WIDTH, HEIGHT);
+
+			float my_color[4];
+			bool my_tool_active;
+
+            while (!done) {
+                SDL_Event event;
+				ImGuiIO& io = ImGui::GetIO();
+                std::clock_t then = std::clock();
+
+				int mouseX, mouseY;
+				const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+				io.DeltaTime = 1.0f / 60.0f;
+				io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
+				io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+				io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+
+				ImGui::NewFrame();
+			
+				// Create a window called "My First Tool", with a menu bar.
+				ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+				if (ImGui::BeginMenuBar())
+				{
+					if (ImGui::BeginMenu("File"))
+					{
+						if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+						if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
+						if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenuBar();
+				}
+
+				// Edit a color (stored as ~4 floats)
+				ImGui::ColorEdit4("Color", my_color);
+				// Plot some values
+				const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
+				ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
+
+				// Display contents in a scrolling region
+				ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
+				ImGui::BeginChild("Scrolling");
+				ImGui::Text("Random Message\n");
+				ImGui::EndChild();
+				ImGui::End();
+
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+				SDL_RenderClear(renderer); // clear previous frame generated image
+
+                cam.render_scene(objects, renderer); // feed triangle data onto renderer
+
+				ImGui::Render();
+				ImGuiSDL::Render(ImGui::GetDrawData());
+                SDL_RenderPresent(renderer); // present the generated triangle data onto screen
+
+                while (SDL_PollEvent(&event)) {
+
+					if( event.type == SDL_KEYDOWN){
+						if( event.key.keysym.sym == SDLK_a )
+							printf("Just pressed keyboard key A!\n");
+					}
+
+					// mouse pressed event
+                    if( event.type == SDL_MOUSEBUTTONDOWN )
+                    {
+                        //If the left mouse button was pressed
+                        if( event.button.button == SDL_BUTTON_RIGHT )
+                        { 
+                        }
+						if( event.button.button == SDL_BUTTON_LEFT)
+						{
+							//Relative mouse mode moves mouse to center of window
+							//every frame. Useful if you want to use mouse and not worry
+							//about it going off screen
+							SDL_SetRelativeMouseMode(SDL_TRUE);
+						}
+                    }
+                    if( event.type == SDL_MOUSEBUTTONUP )
+                    {
+                        //If the left mouse button was released
+                        if( event.button.button == SDL_BUTTON_LEFT )
+                        { 
+							SDL_SetRelativeMouseMode(SDL_FALSE);
+                        }
+                    }
+
+					// mouse move event
+                    if( event.type == SDL_MOUSEMOTION )
+					{
+						//get relative mouse motion, i.e mouse delta x,y from last frame
+						float x = event.motion.xrel;
+                        float y = event.motion.yrel;
+
+                    }
+
+                    if (event.type == SDL_QUIT)
+                        done = SDL_TRUE;
+					
+                }
+            }
+        }
+
+        if (renderer) {
+            SDL_DestroyRenderer(renderer);
+        }
+        if (window) {
+            SDL_DestroyWindow(window);
+        }
+    }
+    SDL_Quit();
+    return 0;
+}
