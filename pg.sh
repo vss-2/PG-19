a6ba54aa2b7a467bb1cec09b004e4c5e2374c27e
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-10-25 17:46:08 -0300

    Add files via upload

---
+++ pg.sh
@@ -0,0 +1,13 @@
+curl https://cin.ufpe.br/~vss2/drive/PG/New_PG%20-%20Linux_MinGW.zip >> 'PG.zip'
+7z x PG.zip
+rm PG.zip
+cd PG\ -\ Linux_MinGW/
+rm -rf camera.h
+rm -rf main.cpp
+
+curl https://raw.githubusercontent.com/vss-2/PG-19/master/main.cpp >> 'main.cpp'
+curl https://raw.githubusercontent.com/vss-2/PG-19/master/camera.h >> 'camera.h'
+curl https://raw.githubusercontent.com/vss-2/PG-19/master/monkey_smooth.obj >> 'monkey_smooth.obj'
+cd ImGUI
+rm -rf imgui_sdl.cpp
+curl https://raw.githubusercontent.com/vss-2/PG-19/e1722148b4bfb7c302f7c5dd588b3a33d2c609f0/ImGUI/imgui_sdl.cpp >> 'imgui_sdl.cpp'
